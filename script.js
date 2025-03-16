429a3440f655a7f95b60b478b97c5540aeb2ccca
ARYA SUKA JKT48 😋 <aryajuli654321@gmail.com>
2025-03-17 05:36:26 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(() => {
-        document.querySelector(".loading-screen").style.display = "none";
-    }, 2000);
-});
-
-function showAlert() {
-    alert("Selamat datang di Perusahaan Elite!");
-}
