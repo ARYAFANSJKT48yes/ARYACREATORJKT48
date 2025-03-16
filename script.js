276984904c8ec8e0c61460b024e667fa5d8a0fd9
ARYA SUKA JKT48 😋 <aryajuli654321@gmail.com>
2025-03-16 07:25:35 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function updateTime() {
-    const timeElement = document.getElementById("time");
-    const now = new Date();
-    const formattedTime = now.toLocaleTimeString("id-ID", { hour: '2-digit', minute: '2-digit', second: '2-digit' });
-    timeElement.textContent = `Waktu saat ini: ${formattedTime}`;
-}
-
-setInterval(updateTime, 1000);
-updateTime();
