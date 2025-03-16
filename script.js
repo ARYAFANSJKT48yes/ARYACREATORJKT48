bc75db9878739737bc3976425bb73970cc9b91d5
ARYA SUKA JKT48 😋 <aryajuli654321@gmail.com>
2025-03-17 05:46:03 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(() => {
-        document.getElementById("loading").style.display = "none";
-    }, 2000);
-});
-
-function showMessage() {
-    alert("Button berhasil diklik!");
-}
