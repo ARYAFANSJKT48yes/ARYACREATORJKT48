ecc947fda3b97927d7a60a527698933e3b4ff521
ARYA SUKA JKT48 😋 <aryajuli654321@gmail.com>
2025-03-17 05:22:09 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(() => {
+        document.querySelector(".loading-screen").style.display = "none";
+    }, 2000);
+});
+
+function showAlert() {
+    alert("Selamat datang di Perusahaan Elite!");
+}
