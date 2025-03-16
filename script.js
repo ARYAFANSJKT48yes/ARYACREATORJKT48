5cdc815ad020c04cfb6532222342c9eb124612ad
ARYA SUKA JKT48 😋 <aryajuli654321@gmail.com>
2025-03-16 08:16:34 +0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let timezoneElement = document.getElementById("timezone");
-    let timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-    timezoneElement.textContent = "Zona Waktu: " + timezone;
-});
