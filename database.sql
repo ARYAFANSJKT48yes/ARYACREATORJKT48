4495c33e49419aeb564efe5a9160d9668f061cf5
ARYA SUKA JKT48 😋 <aryajuli654321@gmail.com>
2025-03-18 06:33:11 +0700

    Delete database.sql

--- database.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS aryawebsite_db;
-USE aryawebsite_db;
-
-CREATE TABLE IF NOT EXISTS visitors (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    ip_address VARCHAR(50),
-    device TEXT,
-    visit_time DATETIME
-);
